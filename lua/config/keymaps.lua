519065f0a49f97e5b24d53798a63a93b75cf6887
van <vexyzy@yandex.ru>
2025-01-17 11:26:31 +0400

    Initialize starter by LazyVim. Add configuration in options.lua and some mappings in the keymaps.lua.

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Navigation in windows
+vim.keymap.set("n", "<CR>l", ":wincmd l<CR>")
---
+++ lua/config/options.lua
@@ -0,0 +1,51 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- Line Numbers
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- indent
+
+-- Mouse
+vim.opt.mouse = "a"
+vim.opt.mousefocus = true
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Indent Settings
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+-- Other
+vim.opt.scrolloff = 8
+vim.opt.wrap = false
+vim.opt.termguicolors = true
+
+-- Fillchars
+vim.opt.fillchars = {
+  vert = "│",
+  fold = "⠀",
+  eob = " ", -- suppress ~ at EndOfBuffer
+  -- diff = "⣿", -- alternatives = ⣿ ░ ─ ╱
+  msgsep = "‾",
+  foldopen = "▾",
+  foldsep = "│",
+  foldclose = "▸",
+}
+
+if vim.g.neovide then
+  vim.g.neovide_window_blurred = true
+  vim.g.neovide_transparency = 0.5
+  vim.g.neovide_show_border = true
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_cursor_vfx_opacity = 1000.0
+end
+
+-- Enable autoindent for all files
+vim.o.autoindent = true
+
+-- Optionally, you could use smartindent for programming languages
+vim.o.smartindent = true
